3bf8d46510022c8974ecc9559ddada5cfa9e638c
Jonathan Zambrano <jrr.zambrano@yavirac.edu.ec>
2022-02-17 21:44:15 -0500

    fix general

--- fron-end/src/app/app-routing.module.ts
+++ fron-end/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CampaniaComponent } from './campania/campania.component';
 import { PermisoComponent } from './permiso/permiso.component';
+import { RolComponent } from './rol/rol.component';
 
 const routes: Routes = [
   {path:'campania',component: CampaniaComponent},
   {path: 'permiso',component: PermisoComponent},
+  {path: 'rol', component: RolComponent}
 ];
 
 @NgModule({
