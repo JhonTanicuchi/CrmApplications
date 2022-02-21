a3e3edf41b9594e82cd4d9ef3bf75490ef5e5305
1753357282 <diq.torres@yavirac.edu.ec>
2022-02-21 00:23:57 -0500

    datos nuevos

--- fron-end/src/app/app-routing.module.ts
+++ fron-end/src/app/app-routing.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CampaniaComponent } from './campania/campania.component';
-import { PermisoComponent } from './permiso/permiso.component';
 import { RolComponent } from './rol/rol.component';
-import { UsuarioComponent } from './usuario/usuario.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'home', component: HomeComponent},
-  {path:'campania',component: CampaniaComponent},
-  {path: 'permiso',component: PermisoComponent},
-  {path: 'rol', component: RolComponent},
-  {path: 'usuario', component: UsuarioComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'rol', component: RolComponent },
 ];
 
 @NgModule({
