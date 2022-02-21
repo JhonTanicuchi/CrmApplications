3715a6b94b91c908b55f81a4bb2d4370454d232c
Jonathan Zambrano <jrr.zambrano@yavirac.edu.ec>
2022-02-21 08:27:31 -0500

    nueva base

--- fron-end/src/app/app-routing.module.ts
+++ fron-end/src/app/app-routing.module.ts
@@ -1,24 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CampaniaComponent } from './campania/campania.component';
-import { PermisoComponent } from './permiso/permiso.component';
 import { RolComponent } from './rol/rol.component';
-import { UsuarioComponent } from './usuario/usuario.component';
+
 import { HomeComponent } from './home/home.component';
-import { EtapaComponent } from './etapa/etapa.component';
-import { ActividadComponent } from './actividad/actividad.component';
-import { CotizacionComponent } from './cotizacion/cotizacion.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
-  {path:'campania',component: CampaniaComponent},
-  {path: 'permiso',component: PermisoComponent},
   {path: 'rol', component: RolComponent},
-  {path: 'usuario', component: UsuarioComponent},
-  {path: 'etapa', component: EtapaComponent},
-  {path: 'actividad', component:ActividadComponent},
-  {path: 'cotizacion', component:CotizacionComponent}
 ];
 
 @NgModule({
--- fron-end/src/app/app.module.ts
+++ fron-end/src/app/app.module.ts
@@ -3,31 +3,21 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ActividadComponent } from './actividad/actividad.component';
 import { MenuComponent } from './compartidos/menu/menu.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { CampaniaComponent } from './campania/campania.component'
 import { RolComponent } from './rol/rol.component';
-import { UsuarioComponent } from './usuario/usuario.component';
 import { HomeComponent } from './home/home.component';
-import { EtapaComponent } from './etapa/etapa.component';
-import { PermisoComponent } from './permiso/permiso.component';
-import { CotizacionComponent } from './cotizacion/cotizacion.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PermisoComponent,
-    CampaniaComponent,
     MenuComponent,
     RolComponent,
     HomeComponent,
-    UsuarioComponent,
-    EtapaComponent,
-    ActividadComponent,
-    CotizacionComponent
+
   ],
   imports: [
     BrowserModule,
