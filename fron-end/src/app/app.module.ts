4f1ac134097f51d03c6aac18748d308a43c53fef
Ricardo Yaguachi <ricky081297@hotmail.com>
2022-02-21 10:46:32 -0500

    fix add primeng

--- fron-end/src/app/app.module.ts
+++ fron-end/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ActividadComponent } from './actividad/actividad.component';
 import { MenuComponent } from './compartidos/menu/menu.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CampaniaComponent } from './campania/campania.component'
 import { RolComponent } from './rol/rol.component';
@@ -15,6 +16,13 @@ import { EtapaComponent } from './etapa/etapa.component';
 import { PermisoComponent } from './permiso/permiso.component';
 import { CotizacionComponent } from './cotizacion/cotizacion.component';
 
+//primeng
+import {ConfirmPopupModule} from 'primeng/confirmpopup';
+import {ConfirmationService} from 'primeng/api';
+import {MessageService} from 'primeng/api';
+import { DialogModule } from 'primeng/dialog';
+import {BlockUIModule} from 'primeng/blockui';
+import {ToastModule} from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -31,12 +39,18 @@ import { CotizacionComponent } from './cotizacion/cotizacion.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     CommonModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    ConfirmPopupModule,
+    BlockUIModule,
+    ToastModule,
+    DialogModule,
   ],
-  providers: [],
+  providers: [ConfirmationService,MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
